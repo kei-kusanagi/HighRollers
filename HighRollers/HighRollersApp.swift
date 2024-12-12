10dabfa3d6c12edb1c256b6aa3c4c18de4aa51c3
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-12-12 16:29:55 -0600

    Initial Commit

---
+++ HighRollers/HighRollersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighRollersApp.swift
+//  HighRollers
+//
+//  Created by Juan Carlos Robledo Morales on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct HighRollersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HighRollersTests/HighRollersTests.swift
@@ -0,0 +1,17 @@
+//
+//  HighRollersTests.swift
+//  HighRollersTests
+//
+//  Created by Juan Carlos Robledo Morales on 12/12/24.
+//
+
+import Testing
+@testable import HighRollers
+
+struct HighRollersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
