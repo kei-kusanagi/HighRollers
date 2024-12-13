459022351c6250a9bd568a83d1951ed1cbf1e528
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-12-12 19:09:00 -0600

    finish the challenge

---
+++ HighRollers/DiceResult.swift
@@ -0,0 +1,30 @@
+//
+//  DiceResult.swift
+//  HighRollers
+//
+//  Created by Juan Carlos Robledo Morales on 12/12/24.
+//
+
+import Foundation
+
+
+struct DiceResult: Identifiable, Codable {
+    var id = UUID()
+    var type: Int
+    var number: Int
+    var rolls = [Int]()
+    
+    var description: String {
+        rolls.map(String.init).joined(separator: ", ")
+    }
+
+    init(type: Int, number: Int) {
+        self.type = type
+        self.number = number
+
+        for _ in 0..<number {
+            let roll = Int.random(in: 1...type)
+            rolls.append(roll)
+        }
+    }
+}
